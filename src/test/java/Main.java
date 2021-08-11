7afbb904e46b8f1c8f1668f6676ef5ffa4dba674
kharaim <akharaim@gmai.com>
2021-08-11 14:53:11 +0300

    new project

---
+++ src/test/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String hello = "Hello!";
+
+        int counter = 0;
+        for (char ch: hello.toLowerCase().toCharArray()) {
+            if (ch == 'o') {
+                counter++;
+            }
+        }
+        System.out.println("Count: " + counter);
+
+    }
+}
